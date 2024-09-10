5a61eb46ee75b7d9a48461abb0997d85d096efec
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-09-10 11:09:56 +0500

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	printf("Enter a number\n");
+	scanf("%d",&num);
+	
+	if (num%2==0)
+	{
+	printf("Even Number");
+	}
+	else
+	{
+	printf("Od number");
+	}
+return 0;
+}
