0a1cccf33de938cf290e2325592ff6130e1f6207
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-09-10 21:42:58 +0500

    Update checkinput.c

--- checkinput.c
+++ checkinput.c
@@ -2,27 +2,29 @@
 
 int main()
 {
-    char a;
+	char a;
 
-   
-    printf("Enter any character: ");
-    scanf("%c", &a);
+	printf("Enter any character: ");
+	scanf("%c", &a);
 
-
-    
-    if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || 
-       a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
-    {
-        printf("'%c' is Vowel.", a);
-    }
-    else if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
-    {
+	if (a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || 
+	    a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+	{
+		printf("'%c' is Vowel.", a);
+	}
+	else if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+	{
         
-        printf("'%c' is Consonant.", a);
-    }
-    else if ("a>=0 && a<=9")
-    {
-    printf("'%c' is a number.", a);
-    }
+		printf("'%c' is Consonant.", a);
+	}
+	else if (a>= '0' && a<= '9')
+	{
+		printf("'%c' is a number.", a);
+	}
+	else
+	{	if ("a=='!' || a=='@' || a=='#' || a=='$' || a=='%' || a=='^' || a=='&' || a=='*' || a=='(' || a==')' a=='+'")
+		printf("'%c' is a special character.", a);
+	}
+
 return 0;
 }
