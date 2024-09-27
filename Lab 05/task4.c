3365efa5f67bcc4dde532137cf91fc3f97b32192
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-09-27 10:39:53 +0500

    Create task4.c

---
+++ Lab 05/task4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+	float a;
+	
+	printf("Enter a number: ");
+	scanf("%f", &a);
+	
+	(a==0)? printf("Number is zero."): (a<0)? printf("Number is negative."): (a>0)? printf("Number is positive."): printf("invalid number");
+return;	
+}
