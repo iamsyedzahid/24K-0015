b7ee5033fd4f64d5479a4be39c9888bf28d5a510
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-09-27 16:03:12 +0500

    Create task9.c

---
+++ Lab 05/task9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	
+	printf("Enter 1st number: ");
+	scanf("%d", &a);
+	
+	printf("Enter 2nd number: ");
+	scanf("%d", &b);
+	
+	//swapping numbers using XOR
+	
+	a = a ^ b; 
+    b = a ^ b;
+    a = a ^ b;
+
+	printf("After Swapping: a = %d, b = %d", a, b);
+
+	return 0;
+}
