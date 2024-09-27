df3f433c23046f5e269ceabc2661f3ab7fd71250
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-09-27 08:58:45 +0500

    Add files via upload

---
+++ Lab 05/task3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b, c;
+	printf("Enter first number: ");
+	scanf("%d", &a);
+	printf("Enter second number: ");
+	scanf("%d", &b);
+	printf("Enter third number: ");
+	scanf("%d", &c);
+	
+	if (a>b && a>c)
+	{ 
+	printf("%d is greatest number.", a);
+	}
+	else if (b>a && b>c)
+	{ 
+	printf("%d is greatest number.", b);
+	}
+	else if (c>b && c>a)
+	{ 
+	printf("%d is greatest number.", c);
+	}
+	else if (a==b && a>c)
+	{ 
+	printf("%d is greatest.", a);
+	}
+	else if (a==c && a>c)
+	{ 
+	printf("%d is greatest.", a);
+	}
+	else if (c==b && c>a)
+	{ 
+	printf("%d is greatest.", c);
+	}
+return;
+}	
+	
+	
