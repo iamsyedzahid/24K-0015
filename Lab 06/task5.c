4e070716e8c5eb7598291e8e59805ec04d7ebbb9
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-05 15:12:21 +0500

    Update task5.c

--- Lab 06/task5.c
+++ Lab 06/task5.c
@@ -2,23 +2,50 @@
 
 int main()
 {
-	int a, i=2;
-	
+	int a, i, j;
+
 	printf("Enter a number till which you need prime numbers: ");
 	scanf("%d", &a);
-	
-	printf("%d ", i);
-	
-	for (i=2;i<a;i++)
+
+	if (a < 2)
 	{
-		if ((i%2 == 0) || (i%3 == 0) || (i%5 == 0) || (i%6 == 0) || (i%7 == 0) || (i%8 == 0) || (i%9 == 0) )
+	printf("No prime numbers.");
+	return 0;
+	} 
+	else if (a == 2)
+	{
+	printf("2 ");
+	return 0;    	}
+
+	//Start from 3 and check for odd numbers only
+	printf("2 "); //Printing the first prime number
+
+	i = 3; //Start checking from 3
+
+	while (i <= a) 
+	{
+	int j = 2; //Check for factors from 2
+
+		//Check if i is prime
+		while (j * j <= i) 
 		{
-			continue;
+			if (i % j == 0)
+			{
+				break; //Not a prime, go out of 2nd while loop
+			}
+		j++; //Check the next possible factor
 		}
-		else
+
+		//If no factors were found, i is prime
+		if (j * j > i) 
 		{
-			printf("%d ", i);
+			printf("%d ", i); //Printing the prime number
 		}
+
+	i += 2;
 	}
+
+	printf("\n");
+
 return 0;
 }
