b532ad9f662f260b05f5bc5ac13a33eeeff2a1ef
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-07 10:45:56 +0500

    Create master.c

---
+++ Assignment/master.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int main() {
+  int a;
+  printf("Enter Problem number, 1 to 13 problems, 0 to exit! :: ");
+  scanf("%d", &a);
+
+      switch (a) {
+    case 1:
+      /* code */
+      break;
+    case 2:
+
+      break;
+    case 3:
+      /* code */
+      break;
+    case 4:
+
+      break;
+    case 5:
+      /* code */
+      break;
+    case 6:
+
+      break;
+    case 7:
+      /* code */
+      break;
+    case 8:
+
+      break;
+    case 9:
+      /* code */
+      break;
+    case 10:
+
+      break;
+    case 11:
+      /* code */
+      break;
+    case 12:
+
+      break;
+    case 13:
+      /* code */
+      break;
+    case 0:
+      printf("Program ended successfully!");
+      break;
+    default:
+      printf("Wrong Input Entered!!\nRerun the program :)");
+      break;
+  }
+
+  return 0;
+}
