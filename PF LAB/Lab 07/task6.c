1182af174f88fbc766ec4f65b7f75dc4880705cf
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-11 01:18:09 +0500

    Update task6.c

--- PF LAB/Lab 07/task6.c
+++ PF LAB/Lab 07/task6.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    int size, i;
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
+    int i;
+    int input;
+
+    printf("Enter how many inputs you need: ");
+    scanf("%d", &input);
+
+    int num[input];
+	
     
-    int arr[size];
-    int count[size];
-
-    for (i = 0; i < size; i++) {
-        count[i] = 0;
+	for (i = 0; i < input; i++)
+    {
+        printf("Element %d: ", i + 1);
+        scanf("\n%d", &num[i]);
     }
-
-    for (i = 0; i < size; i++) {
-        printf("Element %d = ", i + 1);
-        scanf("%d", &arr[i]);
-        count[arr[i]] = count[arr[i]] + 1;
-    }
-
-    printf("The numbers that occur more than once are: ");
-    int found = 0;
-    for (i = 0; i < size; i++) {
-        if (count[i] > 1) {
-            printf("%d ", i);
-            found = 1;
+    
+    int max = num[0];
+    for (i=0 ; i < input ; i++)
+    {
+        if(num[i]>max)
+        {
+            max = num[i];
         }
     }
 
-    if (!found) {
-        printf("No numbers occur more than once.\n");
-    } else {
-        printf("\n");
+	int countarr[max+1];
+	for (i = 0; i <= max; i++)
+    {
+       countarr[i] = 0;
     }
-
+    
+    for (i = 0; i < input; i++)
+    {
+       countarr[num[i]] +=1;
+    }
+    
+    printf("Numbers ocurring more than once: ");
+    for (i = 0; i <= max; i++)
+    {
+       if(countarr[i] > 1){
+       		printf("%d, ", i);
+	}
+    }
     return 0;
 }
