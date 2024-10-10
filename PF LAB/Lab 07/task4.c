a6018c1095f23f686a9562c6d9cbc7cbcd547487
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-10 22:38:32 +0500

    Update task4.c

--- PF LAB/Lab 07/task4.c
+++ PF LAB/Lab 07/task4.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int arr[10], min, max;
 
     printf("Enter 10 numbers:\n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         scanf("%d", &arr[i]);
     }
 
     min = arr[0];
     max = arr[0];
 
-    for (int i = 1; i < 10; i++) {
-        if (arr[i] < min) {
+    for (int i = 1; i < 10; i++)
+    {
+        if (arr[i] < min)
+        {
             min = arr[i];
         }
-        if (arr[i] > max) {
+        if (arr[i] > max)
+        {
             max = arr[i];
         }
     }
