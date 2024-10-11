717657a40a1647823f98f3f81b91e497ef5ccaa9
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-11 09:58:59 +0500

    Create javascript.js

---
+++ ict/javascript.js
@@ -0,0 +1,31 @@
+https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+console.log("HI");
+
+var myname = "Fatima";
+console.log(myname);
+myname = "Ali";
+console.log(myname);
+var myname = "Hassan";
+
+let score = 100;
+console.log(score);
+
+score = 50;
+
+let greetings="Hello";
+if(true){
+    let greetings = "Good Morning";
+    console.log(greetings);
+}
+console.log(greetings);
+
+const pi = 3.141592653589793238462643383279502884197;
+console.log(pi);
+
+console.log("My name is Zahid");
+
+var name;
+var age;
+const user { name : "Zahid" , age : 20};
+user.age = 20;
+console.log(user.age);
