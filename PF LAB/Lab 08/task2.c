8b5eb9c2abe65020b2af020b351f1c4efa0b6870
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-23 20:36:06 +0500

    Create task2.c

---
+++ PF LAB/Lab 08/task2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+	int start, end, num, i;
+
+	printf("Enter the start of the range: ");
+	scanf("%d", &start);
+	printf("Enter the end of the range: ");
+	scanf("%d", &end);
+
+    if (start>end)
+    {
+        printf("Start should be lesser than End");
+        return -1;
+    }
+
+	printf("Prime numbers between %d and %d are:\n", start, end);
+    
+	for (num = start; num <= end; num++)
+	{
+		if (num < 2) continue;
+
+		for (i = 2; i <= num; i++)
+		{
+			if (num % i == 0)
+			{
+				break;
+			}
+		}
+		if (i * i > num)
+		{
+			printf("%d ", num);
+		}
+	}
+
+	printf("\n");
+return 0;
+}
