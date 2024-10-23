5e216624f787a44e33c9c2be6342d7f2a359d0a3
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-23 12:23:02 +0500

    Create task1.c

---
+++ PF LAB/Lab 08/task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int n, i, j;
+  
+	printf("Enter size of square matrix: ");
+	scanf("%d", &n);
+
+  int matrix[n][n];
+
+  printf("Enter matrix elements:\n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+
+	printf("Diagonal elements:\n");
+	for (i = 0; i < n; i++) {
+		printf("%d\n", matrix[i][i]);
+	}
+
+	return 0;
+}
