e3bb77e2c425e3c23facd19736f4a29e2e24d10b
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-30 01:20:48 +0500

    Update task6.c

--- PFAssign2/Task6/task6.c
+++ PFAssign2/Task6/task6.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int N;
+
+    printf("Enter the number of matchsticks: ");
+    scanf("%d", &N);
+
+    if (N % 5 == 0)
+    {
+        printf("It's impossible for A to win.\n");
+        return -1;
+    }
+    else
+    {
+        printf("A should pick %d matchstick(s) and keep leaving multiple of 5 for B to pick to guarantee a win.\n", N % 5);
+        return N%5;
+    }
+}
