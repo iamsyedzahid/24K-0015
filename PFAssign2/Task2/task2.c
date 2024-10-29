18b6e96702664fe6be3585dfbdf8c9b1ae4eb126
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-10-29 21:18:24 +0500

    Update task2.c

--- PFAssign2/Task2/task2.c
+++ PFAssign2/Task2/task2.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main()
+{
+    char slogans[3][100] = {"buy now", "save big", "limited offer"};
+    int count[256];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int k = 0; k < 256; k++)
+        {
+            count[k] = 0;
+        }
+
+        for (int j = 0; slogans[i][j] != '\0'; j++)
+        {
+            char currentChar = slogans[i][j];
+            if (currentChar >= 0 && currentChar < 256)
+            {
+                count[currentChar]++;
+            }
+        }
+
+        printf("Slogan %d: {", i + 1);
+        for (int k = 0; k < 256; k++)
+        {
+            if (count[k] > 0)
+            {
+                printf("'%c': %d, ", k, count[k]);
+            }
+        }
+        printf("\b\b}\n\n");
+    }
+
+    return 0;
+}
