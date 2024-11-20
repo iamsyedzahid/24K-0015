449938724463f98c9ecc869eb2e81bfbcd877156
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-11-20 23:20:51 +0500

    Update task4.c

--- PF LAB/Lab 09/task4.c
+++ PF LAB/Lab 09/task4.c
@@ -1,11 +1,5 @@
 // https://www.w3schools.com/c/c_ref_ctype.php
-/* 
-Create a program that accepts a 2D array of strings (e.g., 5 words with a max length of 20characters each).
-Determines if each word (row) is a palindrome.
-Outputs “Palindrome” or “Not Palindrome” for each word. 
-A palindrome is a word that reads the same forward and backward.
-For example: "madam", "racecar", "level", "radar".
-*/
+
 #include <stdio.h>
 #include <string.h>
 
