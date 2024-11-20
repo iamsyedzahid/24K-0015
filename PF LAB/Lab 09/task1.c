787bcd1435f1fac966865d8ee1a7f22e4a952dd0
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-11-20 22:14:47 +0500

    Update task1.c

--- PF LAB/Lab 09/task1.c
+++ PF LAB/Lab 09/task1.c
@@ -1,1 +1,20 @@
+// Write a C function that takes two numbers as input and returns their product.
 
+#include <stdio.h>
+
+int productOfTwoNum(int a, int b)
+{
+    int product = a * b;
+    printf("Product is %d.", product);
+}
+
+int main()
+{
+    int x,y;
+    printf("Enter the 1st number: ");
+    scanf("%d", &x);
+    printf("Enter the 2nd number: ");
+    scanf("%d", &y);
+    productOfTwoNum(x,y);
+    return 0;
+}
