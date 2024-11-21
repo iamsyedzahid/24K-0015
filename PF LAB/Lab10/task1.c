503f6615e4512a34b228c5a0e323e8a69c7c6ce6
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-11-21 17:22:30 +0500

    Update task1.c

--- PF LAB/Lab10/task1.c
+++ PF LAB/Lab10/task1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int sum_of_digits(int n)
+{
+    if (n < 10) return n;
+    return (n % 10) + sum_of_digits(n / 10);
+}
+
+int main()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("Sum of digits: %d\n", sum_of_digits(number));
+    return 0;
+}
