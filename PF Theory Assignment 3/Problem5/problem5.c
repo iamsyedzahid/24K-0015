8246969d7cc125523a0c68e456d648cb86673e74
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2024-12-13 00:32:59 +0500

    Update problem5.c

--- PF Theory Assignment 3/Problem5/problem5.c
+++ PF Theory Assignment 3/Problem5/problem5.c
@@ -1,1 +1,31 @@
+float calculateAverage(int totalScore, int numPeriods) {
+    return (float)totalScore / numPeriods;
+}
 
+int findEmployeeOfTheYear(Employee* employees, int numEmployees, int numPeriods) {
+    float highestAvg = 0.0;
+    int bestEmployee = -1;
+    
+    for (int i = 0; i < numEmployees; i++) {
+        float avgScore = calculateAverage(employees[i].totalScore, numPeriods);
+        if (avgScore > highestAvg) {
+            highestAvg = avgScore;
+            bestEmployee = i;
+        }
+    }
+    return bestEmployee;
+}
+
+int findWorstPerformingEmployee(Employee* employees, int numEmployees, int numPeriods) {
+    float lowestAvg = 10.0;
+    int worstEmployee = -1;
+    
+    for (int i = 0; i < numEmployees; i++) {
+        float avgScore = calculateAverage(employees[i].totalScore, numPeriods);
+        if (avgScore < lowestAvg) {
+            lowestAvg = avgScore;
+            worstEmployee = i;
+        }
+    }
+    return worstEmployee;
+}
