084acd4c544f1875b5ecdf175715a7f1a50efa3b
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-02-07 12:50:56 +0500

    Add files via upload

---
+++ Practice/class.h
@@ -0,0 +1,11 @@
+#ifndef CLASS.H
+#define CLASS.H
+
+namespace myNameSpace {
+    class myClass{
+        public:
+        void greeting();
+    };
+}
+
+#endif
---
+++ Practice/myClass.cpp
@@ -0,0 +1,16 @@
+#include "class.h"
+#include <iostream>
+
+using namespace myNameSpace;
+using namespace std;
+
+void myClass::greeting()
+{
+    cout<< "Hello Muqeetaaa";
+}
+int main()
+{
+    myClass me;
+    me.greeting();
+    return 0;
+}
